60b761235d3c622f83fcfe31afe9bf1a06d87584
tjwldnjs123 <tjwldnjs12345@naver.com>
2023-06-01 12:48:57 +0900

    Feat: list라우터 설정

---
+++ app/list/page.js
@@ -0,0 +1,13 @@
+export default () => {
+  return (
+    <div>
+      <h4 className="title">상품목록</h4>
+      <div className="food">
+        <h4>상품1 $40</h4>
+      </div>
+      <div className="food">
+        <h4>상품2 $50</h4>
+      </div>
+    </div>
+  );
+};
